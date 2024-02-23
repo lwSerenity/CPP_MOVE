41175fa9ec452559922949505ecd1d3f5986ce2f
lzj <1473120002@qq.com>
2024-02-23 22:15:52 +0800

    123131

--- customer1.cpp
+++ customer1.cpp
@@ -1,39 +1,63 @@
-#include "customer.hpp"
+/* #include "customer.hpp" */
+/* #include <type_traits> */
+/* #include <utility> */
+/* #include <vector> */
+/**/
+/* class Person { */
+/* public: */
+/*   std::string first; */
+/*   std::string last; */
+/*   std::string name = "lz"; */
+/**/
+/* public: */
+/*   Person(std::string f, std::string l) */
+/*       : first{std::move(f)}, last{std::move(l)} {} */
+/*   std::string getname() && { return std::move(name); } */
+/*   const std::string &getname() const & { return name; } */
+/* }; */
+/**/
+/* class C { */
+/* public: */
+/*   void foo() const & { std::cout << "foo() const&\n"; } */
+/*   void foo() && { std::cout << "foo() &&\n"; } */
+/*   void foo() & { std::cout << "foo()&\n"; }; */
+/*   void foo() const && { std::cout << "foo() const&&\n"; } */
+/* }; */
+/* std::string getString() { return std::string{"123"}; } */
+/**/
+/* int main() { */
+/*   getString() = "hello"; */
+/**/
+/*   return 0; */
+/* } */
 #include <iostream>
-#include <type_traits>
-#include <utility>
-#include <vector>
+#include <string>
 
-class Person {
+class MyString {
 public:
-  std::string first;
-  std::string last;
-  std::string name = "lz";
+  MyString(const char *str) : data(str) {}
 
-public:
-  Person(std::string f, std::string l)
-      : first{std::move(f)}, last{std::move(l)} {}
-  std::string getname() && { return std::move(name); }
-  const std::string &getname() const & { return name; }
+  // Copy assignment operator with lvalue reference qualifier
+  MyString &operator=(const MyString &other) & {
+    std::cout << "Using lvalue assignment operator." << std::endl;
+    data = other.data;
+    return *this;
+  }
+
+  void print() const { std::cout << data << std::endl; }
+
+private:
+  std::string data;
 };
 
-class C {
-public:
-  void foo() const & { std::cout << "foo() const&\n"; }
-  void foo() && { std::cout << "foo() &&\n"; }
-  void foo() & { std::cout << "foo()&\n"; };
-  void foo() const && { std::cout << "foo() const&&\n"; }
-};
 int main() {
-  C x;
-  x.foo();
-  C{}.foo();
-  std::move(x).foo();
+  MyString a("Hello");
+  MyString b("World");
 
-  const C cx;
-  cx.foo();
-  std::move(cx).foo();
-  std::string getString();
+  // Using lvalue assignment operator
+  a = b; // This will work
 
+  // Using lvalue assignment operator on temporary objects will result in a
+  // compilation error MyString("NewValue") = b;  // Error
   return 0;
 }
